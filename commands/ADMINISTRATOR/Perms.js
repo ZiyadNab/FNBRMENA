cd8fc0df4b431b60112a10ee41fcb8cf498578e2
ZiyadNab <ziyadnabil050@gmail.com>
2021-03-22 01:28:57 +0300

    First Commit

--- commands/ADMINISTRATOR/Perms.js
+++ commands/ADMINISTRATOR/Perms.js
@@ -40,79 +40,80 @@ module.exports = {
             'MANAGE_EMOJIS',
           ]
 
-          const lang = new Discord.MessageEmbed()
-                .setColor('#BB00EE')
-                .setTitle('Choose a language please')
-                .addFields(
-                    {name: 'Add Perm', value: 'React to :white_check_mark:'},
-                    {name: 'Delete Perm', value: 'React to :negative_squared_cross_mark:'}
-                )
-                const msgReact = await message.channel.send(lang)
-                await msgReact.react('✅')
-                msgReact.react('❎')
-                const filter = (reaction, user) => {
-                    return ['✅', '❎'].includes(reaction.emoji.name) && user.id === message.author.id;
-                };
-                await msgReact.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-                            .then( async collected => {
-                                const reaction = collected.first();
-                                if(reaction.emoji.name === '✅'){
+            var valid = true
+            for(const Perm of args){
+                if(!validPermissions.includes(Perm)){
+                    valid = false
+                }
+            }
 
-                                    var shifted = args.shift();
-                                    var valid = true
-                                    for(const Perm of args){
-                                        if(!validPermissions.includes(Perm)){
-                                            valid = false
-                                        }
-                                    }
-                        
-                                if(valid === true){
-                                    admin.database().ref("ERA's").child("Commands").child(shifted).once('value', async data => {
-                                        if(data.exists()){
-                                            await admin.database().ref("ERA's").child("Commands").child(shifted).child("Perms").set([
-                                                args
-                                            ])
-                                        const done = new Discord.MessageEmbed()
-                                        done.setColor('#BB00EE')
-                                        done.setTitle("The " + args + " permission(s) has been addedd")
-                                        message.channel.send(done)
-                                        }else{
-                                            const errCommand = new Discord.MessageEmbed()
-                                            errCommand.setColor('#BB00EE')
-                                            errCommand.setTitle("The " + shifted + " is not a valid command")
-                                            message.channel.send(errCommand)
-                                        }
-                                    })
-                                }else{
-                                    const err = new Discord.MessageEmbed()
-                                    err.setColor('#BB00EE')
-                                    err.setTitle("The " + args + " is not valid perm please type a valid one")
-                                    message.channel.send(err)
-                                    }
+            if(valid === true){
 
+            const lang = new Discord.MessageEmbed()
+                    .setColor('#BB00EE')
+                    .setTitle('Choose a method')
+                    .addFields(
+                        {name: 'Add Perm', value: 'React to :white_check_mark:'},
+                        {name: 'Delete Perm', value: 'React to :negative_squared_cross_mark:'}
+                    )
+                    const msgReact = await message.channel.send(lang)
+                    await msgReact.react('✅')
+                    msgReact.react('❎')
+                    const filter = (reaction, user) => {
+                        return ['✅', '❎'].includes(reaction.emoji.name) && user.id === message.author.id;
+                    };
+                    await msgReact.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+                                .then( async collected => {
+                                    const reaction = collected.first();
+                                    if(reaction.emoji.name === '✅'){
+
+                                        var shifted = args.shift();
+                                        admin.database().ref("ERA's").child("Commands").child(shifted).once('value', async data => {
+                                            if(data.exists()){
+                                                await admin.database().ref("ERA's").child("Commands").child(shifted).child("Perms").set([
+                                                    args
+                                                ])
+                                            const done = new Discord.MessageEmbed()
+                                            done.setColor('#BB00EE')
+                                            done.setTitle("The " + args + " permission(s) has been addedd")
+                                            message.channel.send(done)
+                                            }else{
+                                                const errCommand = new Discord.MessageEmbed()
+                                                errCommand.setColor('#BB00EE')
+                                                errCommand.setTitle("The " + shifted + " is not a valid command")
+                                                message.channel.send(errCommand)
+                                            }
+                                        })
+
+                                    }
+                                    if(reaction.emoji.name === '❎'){
+                                        admin.database().ref("ERA's").child("Commands").child(args[0]).child("Perms").once('value', async data => {
+                                            if(data.exists()){
+                                                admin.database().ref("ERA's").child("Commands").child(args[0]).child("Perms").remove()
+                                            }else{
+                                                const errCommand = new Discord.MessageEmbed()
+                                                errCommand.setColor('#BB00EE')
+                                                errCommand.setTitle("The " + args[0] + " doesn't have perms")
+                                                message.channel.send(errCommand)
+                                            }
+                                        })
                                 }
-                                if(reaction.emoji.name === '❎'){
-                                    admin.database().ref("ERA's").child("Commands").child(args[0]).child("Perms").once('value', async data => {
-                                        if(data.exists()){
-                                            admin.database().ref("ERA's").child("Commands").child(args[0]).child("Perms").remove()
-                                        }else{
-                                            const errCommand = new Discord.MessageEmbed()
-                                            errCommand.setColor('#BB00EE')
-                                            errCommand.setTitle("The " + args[0] + " doesn't have perms")
-                                            message.channel.send(errCommand)
-                                        }
-                                    })
-                            }
 
-                            msgReact.delete()
-
-                            }).catch(err => {
                                 msgReact.delete()
-                                const error = new Discord.MessageEmbed()
-                                .setColor('#BB00EE')
-                                .setTitle(":regional_indicator_x: Sorry we canceled your process becuase no language has been selected")
-                                message.reply(error)
-                        })
+
+                                }).catch(err => {
+                                    msgReact.delete()
+                                    const error = new Discord.MessageEmbed()
+                                    .setColor('#BB00EE')
+                                    .setTitle(":regional_indicator_x: Sorry we canceled your process becuase no language has been selected")
+                                    message.reply(error)
+                            })
+                        }else{
+                            const err = new Discord.MessageEmbed()
+                            err.setColor('#BB00EE')
+                            err.setTitle("The " + args + " is not valid perm please type a valid one")
+                            message.channel.send(err)
+                            }
 
     },
     

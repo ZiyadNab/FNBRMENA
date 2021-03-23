9207faa83ba308a0e583dc1c19a4baa6c79e427c
ZiyadNab <ziyadnabil050@gmail.com>
2021-03-23 09:52:46 +0300

    First Commit

--- commands/Fortnite/News.js
+++ commands/Fortnite/News.js
@@ -188,6 +188,19 @@ module.exports = {
                                         const length = res.data.messages.length
                                         const layout = 1920 / length
 
+                                        const applyText = (canvas, text) => {
+                                            const ctx = canvas.getContext('2d');
+                                            let fontSize = 60;
+                                            do {
+                                                if(lang === "en"){
+                                                    ctx.font = `${fontSize -= 1}px Burbank Big Condensed`;
+                                                }else if(lang === "ar"){
+                                                    ctx.font = `${fontSize -= 1}px Arabic`;
+                                                }
+                                            } while (ctx.measureText(text).width > layout - 100);
+                                            return ctx.font;
+                                        };
+
                                         for(let i = 0; i < length; i++){
                                             const photo = await Canvas.loadImage(res.data.messages[i].image)
                                             ctx.drawImage(photo,0,0,canvas.width,canvas.height)
@@ -290,6 +303,19 @@ module.exports = {
 
                                         const length = res.data.motds.length
                                         const layout = 1920 / length
+
+                                        const applyText = (canvas, text) => {
+                                            const ctx = canvas.getContext('2d');
+                                            let fontSize = 60;
+                                            do {
+                                                if(lang === "en"){
+                                                    ctx.font = `${fontSize -= 1}px Burbank Big Condensed`;
+                                                }else if(lang === "ar"){
+                                                    ctx.font = `${fontSize -= 1}px Arabic`;
+                                                }
+                                            } while (ctx.measureText(text).width > layout - 100);
+                                            return ctx.font;
+                                        };
 
                                         for(let i = 0; i < length; i++){
                                             const photo = await Canvas.loadImage(res.data.motds[i].image)
@@ -555,6 +581,19 @@ module.exports = {
     
                                             const length = res.data.messages.length
                                             const layout = 1920 / length
+
+                                            const applyText = (canvas, text) => {
+                                                const ctx = canvas.getContext('2d');
+                                                let fontSize = 60;
+                                                do {
+                                                    if(lang === "en"){
+                                                        ctx.font = `${fontSize -= 1}px Burbank Big Condensed`;
+                                                    }else if(lang === "ar"){
+                                                        ctx.font = `${fontSize -= 1}px Arabic`;
+                                                    }
+                                                } while (ctx.measureText(text).width > layout - 100);
+                                                return ctx.font;
+                                            };
     
                                             for(let i = 0; i < length; i++){
                                                 const photo = await Canvas.loadImage(res.data.messages[i].image)
@@ -658,6 +697,19 @@ module.exports = {
     
                                             const length = res.data.motds.length
                                             const layout = 1920 / length
+
+                                            const applyText = (canvas, text) => {
+                                                const ctx = canvas.getContext('2d');
+                                                let fontSize = 60;
+                                                do {
+                                                    if(lang === "en"){
+                                                        ctx.font = `${fontSize -= 1}px Burbank Big Condensed`;
+                                                    }else if(lang === "ar"){
+                                                        ctx.font = `${fontSize -= 1}px Arabic`;
+                                                    }
+                                                } while (ctx.measureText(text).width > layout - 100);
+                                                return ctx.font;
+                                            };
     
                                             for(let i = 0; i < length; i++){
                                                 const photo = await Canvas.loadImage(res.data.motds[i].image)

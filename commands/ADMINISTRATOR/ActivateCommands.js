1bac67f00e1347ee1700148a8bb9e4acad2caf4e
ZiyadNab <ziyadnabil050@gmail.com>
2021-03-27 04:32:18 +0300

    First Commit

--- commands/ADMINISTRATOR/ActivateCommands.js
+++ commands/ADMINISTRATOR/ActivateCommands.js
@@ -16,22 +16,22 @@ module.exports = {
                     admin.database().ref("ERA's").child("Commands").child(args[0]).child("Active").once('value', function (data) {
                         var access = data.val().ON;
                         if(access === "true"){
-                            done.setTitle("The " + args[0] + " command is Enable")
+                            done.setTitle("✅ The " + args[0] + " command is Enable")
                         } else if(access === "false"){
-                            done.setTitle("The " + args[0] + " command is Disable")
+                            done.setTitle("✅ The " + args[0] + " command is Disable")
                         }
                         message.channel.send(done)
                     })
                 }else{
                     const err = new Discord.MessageEmbed()
                     .setColor('#BB00EE')
-                    .setTitle("Make sure that you enter TRUE to enable the command or FALSE to disable it etherwise it will throw an error")
+                    .setTitle(":x: Make sure that you enter TRUE to enable the command or FALSE to disable it etherwise it will throw an error")
                     message.channel.send(err)
                 }
             }else{
                 const error = new Discord.MessageEmbed()
                     .setColor('#BB00EE')
-                    .setTitle("Make sure that you have entered a valid command")
+                    .setTitle(":x: Make sure that you have entered a valid command")
                     message.channel.send(error)
             }
         })
--- commands/ADMINISTRATOR/announce.js
+++ commands/ADMINISTRATOR/announce.js
@@ -11,7 +11,17 @@ module.exports = {
         messageAnnounce.setColor('#BB00EE')
         messageAnnounce.setTitle(text)
         const accounce = client.channels.cache.find(channel => channel.id === config.channels.announce)
-        accounce.send(messageAnnounce)
+        if(accounce.send(messageAnnounce)){
+            const messageAnnounceDone = new Discord.MessageEmbed()
+            messageAnnounceDone.setColor('#BB00EE')
+            messageAnnounceDone.setTitle("✅ The announcement has been published")
+            message.channel.send(messageAnnounceDone)
+        }else{
+            const messageAnnounceNotDone = new Discord.MessageEmbed()
+            messageAnnounceNotDone.setColor('#BB00EE')
+            messageAnnounceNotDone.setTitle(":x: There is an error publishing this announcement")
+            message.channel.send(messageAnnounceNotDone)
+        }
     },
     requiredRoles: []
 }

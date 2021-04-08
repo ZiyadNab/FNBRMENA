94d425d3dde9a66f27c5a4e6a8a4ba5a9797e2f0
ZiyadNab <ziyadnabil050@gmail.com>
2021-04-09 01:45:17 +0300

    First Commit

--- commands/Fortnite/UpcomingCosmetics.js
+++ commands/Fortnite/UpcomingCosmetics.js
@@ -34,7 +34,7 @@ module.exports = {
                     const generating = new Discord.MessageEmbed()
                     generating.setColor('#BB00EE')
                     const emoji = client.emojis.cache.get("805690920157970442")
-                    generating.setTitle(`${loading} ${$res.length} ${cosmetics}... ${emoji}`)
+                    generating.setTitle(`${loading} ${res.length} ${cosmetics}... ${emoji}`)
                     message.channel.send(generating)
                     .then( async msg => {
 

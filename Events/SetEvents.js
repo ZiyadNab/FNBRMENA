7daa0fdb7ae4fcf9d139f61ffb8c22f5ee8537ad
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2021-05-18 23:55:34 +0300

    added set event

---
+++ Events/SetEvents.js
@@ -0,0 +1,50 @@
+const FortniteAPI = require("fortniteapi.io-api");
+const Discord = require('discord.js')
+const moment = require('moment')
+const key = require('../Coinfigs/config.json')
+const fortniteAPI = new FortniteAPI(key.apis.fortniteio);
+const Canvas = require('canvas');
+
+module.exports = (client, admin) => {
+    const message = client.channels.cache.find(channel => channel.id === key.events.Set)
+    //result
+    var data = []
+    var lang = "en"
+    var number = 0
+    var sets = ''
+    var counter = 0
+
+    const Set = async () => {
+        fortniteAPI.listSets(options = {lang: lang})
+        .then(async res => {
+            if(number === 0){
+                data = res.sets
+                number++
+            }
+            if(JSON.stringify(res.sets) !== JSON.stringify(data)){
+                for(let i = 0; i < res.sets.length; i++){
+                    if(JSON.stringify(data[i]) !== JSON.stringify(res.sets[i])){
+                        counter++
+                        sets += '\n• '+counter+': '+res.sets[i].name
+                    }
+                }
+                if(lang === "en"){
+                    sets += '\n\n• '+counter+' Sets in total'
+                }else if(lang === "ar"){
+                    sets += '\n\n• المجموع '+counter+' مجموعة'
+                }
+                const setInfo = new Discord.MessageEmbed()
+                setInfo.setColor('#BB00EE')
+                if(lang === "en"){
+                    setInfo.setTitle("All new sets in this update")
+                }else if(lang === "ar"){
+                    setInfo.setTitle("جميع المجموعات الجديدة في التحديث الحالي")
+                }
+                setInfo.setDescription(sets)
+                message.send(setInfo)
+                data = res.sets
+            }
+        })
+    }
+    setInterval(Set, 1 * 60000)
+}

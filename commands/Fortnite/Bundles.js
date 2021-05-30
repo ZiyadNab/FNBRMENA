b59d663324ba0c3adc747ed278b30fc307057b7e
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2021-05-30 17:57:07 +0300

    added bundles

---
+++ commands/Fortnite/Bundles.js
@@ -0,0 +1,136 @@
+const FortniteAPI = require("fortniteapi.io-api");
+const moment = require("moment");
+const key = require('../../Coinfigs/config.json')
+const fortniteAPI = new FortniteAPI(key.apis.fortniteio);
+
+module.exports = {
+    commands: 'bundle',
+    expectedArgs: '',
+    minArgs: 1,
+    maxArgs: null,
+    cooldown: 10,
+    permissionError: 'Sorry you do not have acccess to this command',
+    callback: async (message, args, text, Discord, client, admin, alias, errorEmoji, checkEmoji) => {
+
+        admin.database().ref("ERA's").child("Users").child(message.author.id).once('value', async function (data) {
+            var lang = data.val().lang;
+
+            const offerID = await fortniteAPI.getBundles()
+            .then(async res => {
+
+                //filtering
+                const id = res.bundles.filter(obj => {
+                    return obj.name.toLowerCase() === text.toLowerCase()
+                })
+
+                return id[0].offerId
+            })
+
+            if(offerID.length !== 0){
+
+                //message
+                var mess
+
+                if(lang === "en"){
+                    mess = "Give just a sec to get the bundle data"
+                }else if(lang === "ar"){
+                    mess = "عطني وقت بس قاعد اجيب معلومات الحزمة"
+                }
+
+                const generating = new Discord.MessageEmbed()
+                generating.setColor('#BB00EE')
+                const emoji = client.emojis.cache.get("805690920157970442")
+                generating.setTitle(`${mess} ${emoji}`)
+                message.channel.send(generating)
+                .then( async msg => {
+
+                    fortniteAPI.getBundles(options = {lang: lang})
+                    .then(async res => {
+
+                        //filtering
+                        const found = res.bundles.filter(obj => {
+                            return obj.offerId === offerID
+                        })
+
+                        if(found.length !== 0){
+
+                            //creating embed
+                            const bundle = new Discord.MessageEmbed()
+
+                            //add color
+                            bundle.setColor()
+
+                            //add title
+                            bundle.setTitle(found[0].name)
+
+                            //add description
+                            bundle.setDescription(found[0].description)
+
+                            //payable? and dates
+                            moment.locale(lang)
+                            if(lang === "en"){
+                                if(found[0].available === true){
+                                    bundle.addFields(
+                                        {name: "Available", value: "Yes!"},
+                                        {name: "Available Since", value: moment(found[0].viewableDate).format("dddd, MMMM Do of YYYY")},
+                                        {name: "Will be gone at", value: moment(found[0].expiryDate).format("dddd, MMMM Do of YYYY")}
+                                    )
+                                }else{
+                                    bundle.addFields(
+                                        {name: "Available", value: "No!"},
+                                        {name: "Available since", value: moment(found[0].viewableDate).format("dddd, MMMM Do of YYYY")},
+                                        {name: "Gone since", value: moment(found[0].expiryDate).format("dddd, MMMM Do of YYYY")}
+                                    )
+                                }
+                            }else if(lang === "ar"){
+                                if(found[0].available === true){
+                                    bundle.addFields(
+                                        {name: "متاحة للشراء", value: "لا"},
+                                        {name: "متاحة منذ", value: moment(found[0].viewableDate).format("dddd, MMMM Do من YYYY")},
+                                        {name: "سوف تغادر في", value: moment(found[0].expiryDate).format("dddd, MMMM Do من YYYY")}
+                                    )
+                                }else{
+                                    bundle.addFields(
+                                        {name: "متاحة للشراء", value: "نعم",},
+                                        {name: "متاحة منذ", value: moment(found[0].viewableDate).format("dddd, MMMM Do من YYYY")},
+                                        {name: "غادرت منذ", value: moment(found[0].expiryDate).format("dddd, MMMM Do من YYYY")}
+                                    )
+                                }
+                            }
+
+                            //prices
+                            for(let i = 0; i < found[0].prices.length; i++){
+                                bundle.addFields(
+                                    {name: found[0].prices[i].paymentCurrencyCode, value: found[0].prices[i].paymentCurrencyAmountNatural + found[0].prices[i].paymentCurrencySymbol, inline: true}
+                                )
+                            }
+
+                            //tumbnail
+                            bundle.setThumbnail(found[0].displayAssets[0].url)
+
+                            //set the image
+                            bundle.setImage(found[0].thumbnail)
+
+                            message.channel.send(bundle)
+                            msg.delete()
+                        }else{
+                            console.log("err")
+                        }
+                    })
+                })
+            }else{
+                if(lang === "en"){
+                    const Err = new Discord.MessageEmbed()
+                    .setColor('#BB00EE')
+                    .setTitle(`No bundle has been found check your speling and try again ${errorEmoji}`)
+                    message.reply(Err)
+                }else if(lang === "ar"){
+                    const Err = new Discord.MessageEmbed()
+                    .setColor('#BB00EE')
+                    .setTitle(`لا يمكنني العثور على الحزمة الرجاء التأكد من كتابة الاسم بشكل صحيح ${errorEmoji}`)
+                    message.reply(Err)
+                }
+            }
+        })
+    }
+}
--- commands/Fortnite/landing.js
+++ commands/Fortnite/landing.js
@@ -21,77 +21,154 @@ module.exports = {
 
                 //variables
                 var loading = ""
-                var loop = 1
+                var reply = ""
 
-                //inisilizing loading
+                //inisilizing loading and reply
                 if(lang === "en"){
                     loading = "Getting a list of poi's"
+                    reply = "The command randomly pick a POI if u don't know where to drop!"
                 }else if(lang === "ar"){
                     loading = "جاري تحميل جميع المناطق"
+                    reply = "اذا كنت ما تعرف وين تنزل اكتب الامر خلي البوت بعلمك ممكن تفوز ما تدري"
                 }
 
-                //generating animation
-                const generating = new Discord.MessageEmbed()
-                generating.setColor('#BB00EE')
-                const emoji = client.emojis.cache.get("805690920157970442")
-                generating.setTitle(`${loading} ${res.list.length} ${emoji}`)
-                message.channel.send(generating)
-                .then( async msg => {
+                const picker = async () => {
+                    //generating animation
+                    const generating = new Discord.MessageEmbed()
+                    generating.setColor('#BB00EE')
+                    const emoji = client.emojis.cache.get("805690920157970442")
+                    generating.setTitle(`${loading} ${res.list.length} ${emoji}`)
+                    message.channel.send(generating)
+                    .then( async msg => {
+                        //creating embed
+                        const pickedAgain = new Discord.MessageEmbed()
 
-                    //get a random number
-                    var randomImage = Math.floor(Math.random() * res.list.length)
-                    
-                    //creating embed
-                    const picked = new Discord.MessageEmbed()
-                    
-                    //create the color
-                    picked.setColor('#BB00EE')
+                        //get a random number
+                        var randomImage = Math.floor(Math.random() * res.list.length)
+                        
+                        //create the color
+                        await pickedAgain.setColor('#BB00EE')
 
-                    //set title
-                    if(lang === "en"){
-                        picked.setTitle("Landing Picker")
-                    }else if(lang === "ar"){
-                        picked.setTitle("النزول العشوائي")
-                    }
+                        //set title
+                        if(lang === "en"){
+                            await pickedAgain.setTitle("Landing Picker")
+                        }else if(lang === "ar"){
+                            await pickedAgain.setTitle("النزول العشوائي")
+                        }
 
-                    //set description
-                    if(lang === "en"){
-                        picked.setDescription("you are gonna land at **" + res.list[randomImage].name + "**")
-                    }else if(lang === "ar"){
-                        picked.setDescription("راح تنزل في منطقة **" + res.list[randomImage].name + "**")
-                    }
+                        //set description
+                        if(lang === "en"){
+                            await pickedAgain.setDescription("you are gonna land at **" + res.list[randomImage].name + "**")
+                        }else if(lang === "ar"){
+                            await pickedAgain.setDescription("راح تنزل في منطقة **" + res.list[randomImage].name + "**")
+                        }
 
-                    //Registering fonts
-                    Canvas.registerFont('./assets/font/BurbankBigCondensed-Black.otf' ,{family: 'Burbank Big Condensed',weight: "700",style: "bold"})
-                    Canvas.registerFont('./assets/font/Lalezar-Regular.ttf', {family: 'Arabic',weight: "700",style: "bold"});
+                        //Registering fonts
+                        Canvas.registerFont('./assets/font/BurbankBigCondensed-Black.otf' ,{family: 'Burbank Big Condensed',weight: "700",style: "bold"})
+                        Canvas.registerFont('./assets/font/Lalezar-Regular.ttf', {family: 'Arabic',weight: "700",style: "bold"});
 
-                    //canvas
-                    const canvas = Canvas.createCanvas(1920, 1080);
-                    const ctx = canvas.getContext('2d');
+                        //canvas
+                        const canvas = Canvas.createCanvas(1920, 1080);
+                        const ctx = canvas.getContext('2d');
 
-                    //background
-                    const background = await Canvas.loadImage(res.list[randomImage].images[0].url)
-                    ctx.drawImage(background, 0, 0, 1920, 1080)
+                        //background
+                        const background = await Canvas.loadImage(res.list[randomImage].images[0].url)
+                        ctx.drawImage(background, 0, 0, 1920, 1080)
 
-                    //add blue fog
-                    const fog = await Canvas.loadImage('./assets/News/fog.png')
-                    ctx.drawImage(fog,0,0,1920,1080)
+                        //add blue fog
+                        const fog = await Canvas.loadImage('./assets/News/fog.png')
+                        ctx.drawImage(fog,0,0,1920,1080)
 
-                    //credits
-                    ctx.fillStyle = '#ffffff';
-                    ctx.textAlign='left';
-                    ctx.font = '60px Burbank Big Condensed'
-                    ctx.fillText("FNBRMENA", 15, 55)
+                        //credits
+                        ctx.fillStyle = '#ffffff';
+                        ctx.textAlign='left';
+                        ctx.font = '60px Burbank Big Condensed'
+                        ctx.fillText("FNBRMENA", 15, 55)
 
-                    //encoding...
-                    const att = new Discord.MessageAttachment(canvas.toBuffer(), res.list[randomImage].name + '.png')
+                        //encoding...
+                        const att = new Discord.MessageAttachment(canvas.toBuffer(), res.list[randomImage].name + '.png')
 
-                    //set the image
-                    picked.attachFiles([att])
-                    picked.setImage('attachment://' + att);
+                        //set the image
+                        await pickedAgain.attachFiles([att])
+                        await pickedAgain.setImage('attachment://' + att);
 
-                    loop = 0
-                    message.channel.send(picked)
+                        //creating button
+                        let again = new MessageButton()
+
+                        //button style
+                        again.setStyle('blurple')
+
+                        //button label
+                        if(lang === "en"){
+                            again.setLabel('Try Again!')
+                        }else if(lang === "ar"){
+                            again.setLabel('محاولة اخرى!')
+                        }
+
+                        //button id
+                        again.setID('again');
+
+                        //creating button
+                        let stop = new MessageButton()
+
+                        //button style
+                        stop.setStyle('red')
+
+                        //button label
+                        if(lang === "en"){
+                            stop.setLabel('Stop!')
+                        }else if(lang === "ar"){
+                            stop.setLabel('ايقاف!')
+                        }
+
+                        //button id
+                        stop.setID('stop');
+
+                        await message.channel.send("", {buttons: [again, stop], embed: pickedAgain})
+                        .then(b => {
+                            client.on('clickButton', async (button) => {
+                                //... my code
+                                if(button.id === "again"){
+                                    b.delete()
+                                }
+                                if(button.id === "stop"){
+                                    b.delete()
+                                }
+                            })
+                        })
+                        msg.delete()
+                    })
+                }
+
+                //creating button
+                let start = new MessageButton()
+
+                //button style
+                start.setStyle('blurple')
+
+                //button label
+                if(lang === "en"){
+                    start.setLabel('Start!')
+                }else if(lang === "ar"){
+                    start.setLabel('ابدا!')
+                }
+
+                //button id
+                start.setID('start');
+
+                //send the button
+                await message.channel.send(reply, start)
+                .then(async clicked => {
+                    client.on('clickButton', async (button) => {
+                        //... my code
+                        if(button.id === "start"){
+                            await picker()
+                            clicked.delete()
+                        }
+                        if(button.id === "again"){
+                            await picker()
+                        }
+                    })
                 })
             })
         })

6de93787578ce6223419f38d323cbf95682eb227
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2021-05-31 14:03:46 +0300

    fixed bundles not yet added

--- Events/Events.js
+++ Events/Events.js
@@ -4,7 +4,7 @@ const FNBRMENA = new Data()
 module.exports = async (client, admin, array) => {
 
     FNBRMENA.Events(client, admin, array, "Blogposts")
-    FNBRMENA.Events(client, admin, array, "Bundle")
+    //FNBRMENA.Events(client, admin, array, "Bundle")
     FNBRMENA.Events(client, admin, array, "Comp")
     FNBRMENA.Events(client, admin, array, "Itemshop")
     FNBRMENA.Events(client, admin, array, "Playlists")

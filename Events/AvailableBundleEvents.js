c8c141cd07202ac4458dd582f94c02c1deacc014
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2021-06-23 17:53:09 +0300

    fixed all bundles

--- Events/AvailableBundleEvents.js
+++ Events/AvailableBundleEvents.js
@@ -8,6 +8,7 @@ module.exports = (client, admin) => {
 
     //result
     var response = []
+    var offer = []
     var available = []
     var number = 0
 
@@ -22,16 +23,16 @@ module.exports = (client, admin) => {
             //if the event is set to be true [ON]
             if(status === "true"){
 
-                axios.get('https://fortniteapi.io/v2/bundles?lang=' + lang, { headers: {'Content-Type': 'application/json','Authorization': "d4ce1562-839ff66b-3946ccb6-438eb9cf",} })
+                axios.get(`https://fortniteapi.io/v2/bundles?lang=${lang}&available=true`, { headers: {'Content-Type': 'application/json','Authorization': "d4ce1562-839ff66b-3946ccb6-438eb9cf",} })
                 .then(async res => {
 
                     //store data when the bot if on
                     if(number === 0){
 
-                        //filter and store only available bundles
-                        response = await res.data.bundles.filter(bundle => {
-                            return bundle.available === true
-                        })
+                        //store only available bundles
+                        for(let i = 0; i < res.data.bundles.length; i++){
+                            response[i] = await res.data.bundles[i].offerId
+                        }
 
                         number++
                     }
@@ -41,19 +42,23 @@ module.exports = (client, admin) => {
                         response = []
                     }
 
-                    //get the avaliable bundles att
-                    available = await res.data.bundles.filter(bundle => {
-                        return bundle.available === true
-                    })
+                    //store only available bundles
+                    for(let i = 0; i < res.data.bundles.length; i++){
+                        offer[i] = await res.data.bundles[i].offerId
+                    }
 
                     //compare diff
-                    if(JSON.stringify(available) !== JSON.stringify(response)){
+                    if(JSON.stringify(offer) !== JSON.stringify(response)){
 
                         //diff has been found now loop throw all the available bundles
-                        for(let i = 0; i < available.length; i++){
+                        for(let i = 0; i < offer.length; i++){
 
                             //if there is a new avaliable bundle
-                            if(!response.includes(available[i])){
+                            if(!response.includes(offer[i])){
+
+                                available = await res.data.bundles.filter(id => {
+                                    return id.offerId === offer[i]
+                                })
 
                                 //creating embed
                                 const bundle = new Discord.MessageEmbed()
@@ -62,69 +67,93 @@ module.exports = (client, admin) => {
                                 bundle.setColor('#BB00EE')
 
                                 //add title
-                                bundle.setTitle(available[i].name)
+                                bundle.setTitle(available[0].name)
 
                                 //add description
-                                bundle.setDescription(available[i].description)
+                                bundle.setDescription(available[0].description)
 
                                 //moment language
                                 moment.locale(lang)
 
                                 //add the dates
                                 if(lang === "en"){
-                                    if(available[i].expiryDate !== null){
+                                    if(available[0].expiryDate !== null){
                                         bundle.addFields(
                                             {name: "Available", value: "Yes!"},
-                                            {name: "Available Since", value: moment(available[i].viewableDate).format("dddd, MMMM Do of YYYY")},
-                                            {name: "Will be gone at", value: moment(available[i].expiryDate).format("dddd, MMMM Do of YYYY")}
+                                            {name: "Available Since", value: moment(available[0].viewableDate).format("dddd, MMMM Do of YYYY")},
+                                            {name: "Will be gone at", value: moment(available[0].expiryDate).format("dddd, MMMM Do of YYYY")}
                                         )
                                     }else{
                                         bundle.addFields(
                                             {name: "Available", value: "Yes!"},
-                                            {name: "Available Since", value: moment(available[i].viewableDate).format("dddd, MMMM Do of YYYY")},
-                                            {name: "Will be gone at", value: "No yet known"}
+                                            {name: "Available Since", value: moment(available[0].viewableDate).format("dddd, MMMM Do of YYYY")},
+                                            {name: "Will be gone at", value: "Not yet known"}
                                         )
                                     }
                                 }else if(lang === "ar"){
-                                    if(available[i].expiryDate !== null){
+                                    if(available[0].expiryDate !== null){
                                         bundle.addFields(
                                             {name: "متاحة للشراء", value: "نعم"},
-                                            {name: "متاحة منذ", value: moment(available[i].viewableDate).format("dddd, MMMM Do من YYYY")},
-                                            {name: "سوف تغادر في", value: moment(available[i].expiryDate).format("dddd, MMMM Do من YYYY")}
+                                            {name: "متاحة منذ", value: moment(available[0].viewableDate).format("dddd, MMMM Do من YYYY")},
+                                            {name: "سوف تغادر في", value: moment(available[0].expiryDate).format("dddd, MMMM Do من YYYY")}
                                         )
                                     }else{
                                         bundle.addFields(
                                             {name: "متاحة للشراء", value: "نعم"},
-                                            {name: "متاحة منذ", value: moment(available[i].viewableDate).format("dddd, MMMM Do من YYYY")},
+                                            {name: "متاحة منذ", value: moment(available[0].viewableDate).format("dddd, MMMM Do من YYYY")},
                                             {name: "سوف تغادر في", value: "لا يوجد تاريخ معلوم حتى الان"}
                                         )
                                     }
                                 }
 
+                                //add cutsom prices SAR
+                                var pricesSA = {
+                                    "paymentCurrencyCode": "SAR",
+                                    "paymentCurrencySymbol": "SR",
+                                    "paymentCurrencyAmountNatural": parseFloat(available[0].prices[1].paymentCurrencyAmountNatural * 3.75).toFixed(2)
+                                }
+
+                                //add sar
+                                bundle.addFields(
+                                    {name: pricesSA.paymentCurrencyCode, value: pricesSA.paymentCurrencyAmountNatural + pricesSA.paymentCurrencySymbol, inline: true}
+                                )
+
+                                //add cutsom prices KWD
+                                var pricesKWD = {
+                                    "paymentCurrencyCode": "KWD",
+                                    "paymentCurrencySymbol": "KD",
+                                    "paymentCurrencyAmountNatural": parseFloat(available[0].prices[1].paymentCurrencyAmountNatural * 0.30).toFixed(2)
+                                }
+
+                                //add kwd
+                                bundle.addFields(
+                                    {name: pricesKWD.paymentCurrencyCode, value: pricesKWD.paymentCurrencyAmountNatural + pricesKWD.paymentCurrencySymbol, inline: true}
+                                )
+
                                 //prices
-                                for(let p = 0; p < available[i].prices.length; p++){
+                                for(let p = 0; p < available[0].prices.length; p++){
                                     bundle.addFields(
-                                        {name: available[i].prices[p].paymentCurrencyCode, value: available[i].prices[i].paymentCurrencyAmountNatural + available[i].prices[i].paymentCurrencySymbol, inline: true}
+                                        {name: available[0].prices[p].paymentCurrencyCode, value: available[0].prices[p].paymentCurrencyAmountNatural + available[0].prices[p].paymentCurrencySymbol, inline: true}
                                     )
                                 }
 
                                 //tumbnail and image
-                                if(available[i].displayAssets.length !== 0){
+                                if(available[0].displayAssets.length !== 0){
 
                                     //add thumbnail
-                                    bundle.setThumbnail(available[i].displayAssets[0].url)
+                                    bundle.setThumbnail(available[0].displayAssets[0].url)
 
                                     //add the image
-                                    if(available[i].thumbnail !== null){
-                                        bundle.setImage(available[i].thumbnail)
+                                    if(available[0].thumbnail !== null){
+                                        bundle.setImage(available[0].thumbnail)
                                     }else{
-                                        available[i].displayAssets[0].background
+                                        bundle.setImage(available[0].displayAssets[0].background)
                                     }
                                 }else{
 
                                     //add the image
-                                    if(available[i].thumbnail !== null){
-                                        bundle.setImage(available[i].thumbnail)
+                                    if(available[0].thumbnail !== null){
+                                        bundle.setImage(available[0].thumbnail)
                                     }
                                 }
 
@@ -132,7 +161,11 @@ module.exports = (client, admin) => {
                             
                             }
                         }
-                        response = await available
+
+                        //store only available bundles
+                        for(let i = 0; i < res.data.bundles.length; i++){
+                            response[i] = await res.data.bundles[i].offerId
+                        }
 
                         //trun off push if enabled
                         admin.database().ref("ERA's").child("Events").child("bundles").update({
@@ -143,5 +176,5 @@ module.exports = (client, admin) => {
             }
         })
     }
-    setInterval(Bundle, 0.5 * 60000)
+    setInterval(Bundle, 1 * 20000)
 }
--- commands/Fortnite/Bundles.js
+++ commands/Fortnite/Bundles.js
@@ -16,18 +16,125 @@ module.exports = {
         //get the user language from the database
         const lang = await FNBRMENA.Admin(admin, message, "", "Lang")
 
+        //inisilizing number
+        var num = 0
+
         const offerID = await fortniteAPI.getBundles()
         .then(async res => {
 
             //filtering
             const id = res.bundles.filter(obj => {
-                return obj.name.toLowerCase() === text.toLowerCase()
+                return obj.name.toLowerCase().includes(text.toLowerCase())
             })
 
-            return id[0].offerId
+            //ask the user what bundles he means
+            if(await id.length > 1){
+
+                //create embed
+                const bundles = new Discord.MessageEmbed()
+
+                //add the color
+                bundles.setColor('#BB00EE')
+
+                //create and fill a string of names
+                var str = ""
+                for(let i = 0; i < id.length; i++){
+                    str += '• ' + (i + 1) + ': ' + id[i].name + '\n'
+                }
+
+                //add description
+                bundles.setDescription(str)
+
+                //send the choices
+                await message.channel.send(bundles)
+                .then( async msg => {
+
+                    //filtering
+                    const filter = m => m.author.id === message.author.id
+
+                    //send the reply to the user
+                    if(lang === "en"){
+                        reply = "please choose from above list the command will stop listen in 20 sec"
+                    }else if(lang === "ar"){
+                        reply = "الرجاء الاختيار من القائمة بالاعلى، سوف ينتهي الامر خلال ٢٠ ثانية"
+                    }
+
+                    //send the reply
+                    await message.reply(reply)
+                    .then( async notify => {
+
+                        //await messages
+                        await message.channel.awaitMessages(filter, {max: 1, time: 20000})
+                            .then( async collected => {
+
+                            if(await collected.first().content >= 0 && collected.first().content < id.length){
+
+                                console.log(collected.first().content)
+                                msg.delete()
+                                notify.delete()
+                                num = await collected.first().content
+
+                            }else{
+                                if(lang === "en"){
+                                    msg.delete()
+                                    notify.delete()
+                                    const error = new Discord.MessageEmbed()
+                                    .setColor('#BB00EE')
+                                    .setTitle(`Sorry we canceled your process becuase u selected a number out of range ${errorEmoji}`)
+                                    message.reply(error)
+                                }else if(lang === "ar"){
+                                    msg.delete()
+                                    notify.delete()
+                                    const error = new Discord.MessageEmbed()
+                                    .setColor('#BB00EE')
+                                    .setTitle(`تم ايقاف الامر بسبب اختيارك لرقم خارج النطاق ${errorEmoji}`)
+                                    message.reply(error)
+                                }
+                            }
+                        }).catch(err => {
+                            if(lang === "en"){
+                                notify.delete()
+                                msg.delete()
+                                const error = new Discord.MessageEmbed()
+                                .setColor('#BB00EE')
+                                .setTitle(`Sorry we canceled your process becuase no method has been selected ${errorEmoji}`)
+                                message.reply(error)
+                            }else if(lang === "ar"){
+                                msg.delete()
+                                notify.delete()
+                                const error = new Discord.MessageEmbed()
+                                .setColor('#BB00EE')
+                                .setTitle(`تم ايقاف الامر بسبب عدم اختيارك لطريقة ${errorEmoji}`)
+                                message.reply(error)
+                            }
+                        })
+                    })
+                })
+                return id[num].offerId
+            }
+
+            //there is onle one bundle
+            if(id.length === 1){
+                return id[num].offerId
+            }
+            
+            //no bundle has been found
+            if(id.length === 0){
+                if(lang === "en"){
+                    const Err = new Discord.MessageEmbed()
+                    .setColor('#BB00EE')
+                    .setTitle(`No bundle has been found check your speling and try again ${errorEmoji}`)
+                    message.reply(Err)
+                }else if(lang === "ar"){
+                    const Err = new Discord.MessageEmbed()
+                    .setColor('#BB00EE')
+                    .setTitle(`لا يمكنني العثور على الحزمة الرجاء التأكد من كتابة الاسم بشكل صحيح ${errorEmoji}`)
+                    message.reply(Err)
+                }
+            }
         })
 
-        if(offerID.length !== 0){
+        if(offerID){
 
             //message
             var mess
@@ -49,27 +156,30 @@ module.exports = {
                 .then(async res => {
 
                     //filtering
-                    const found = res.bundles.filter(obj => {
+                    const found = await res.bundles.filter(obj => {
                         return obj.offerId === offerID
                     })
 
-                    if(found.length !== 0){
+                    //creating embed
+                    const bundle = new Discord.MessageEmbed()
 
-                        //creating embed
-                        const bundle = new Discord.MessageEmbed()
+                    //add color
+                    bundle.setColor('#BB00EE')
 
-                        //add color
-                        bundle.setColor('#BB00EE')
+                    //add title
+                    bundle.setTitle(found[0].name)
 
-                        //add title
-                        bundle.setTitle(found[0].name)
+                    //add description
+                    bundle.setDescription(found[0].description)
 
-                        //add description
-                        bundle.setDescription(found[0].description)
+                    //payable? and dates
+                    moment.locale(lang)
+                    if(lang === "en"){
 
-                        //payable? and dates
-                        moment.locale(lang)
-                        if(lang === "en"){
+                        //if there is no expire date
+                        if(found[0].expiryDate !== null){
+
+                            //if the bundle is available
                             if(found[0].available === true){
                                 bundle.addFields(
                                     {name: "Available", value: "Yes!"},
@@ -83,7 +193,27 @@ module.exports = {
                                     {name: "Gone since", value: moment(found[0].expiryDate).format("dddd, MMMM Do of YYYY")}
                                 )
                             }
-                        }else if(lang === "ar"){
+                        }else{
+                            if(found[0].available === true){
+                                bundle.addFields(
+                                    {name: "Available", value: "Yes!"},
+                                    {name: "Available Since", value: moment(found[0].viewableDate).format("dddd, MMMM Do of YYYY")},
+                                    {name: "Will be gone at", value: "Not yet known"}
+                                )
+                            }else{
+                                bundle.addFields(
+                                    {name: "Available", value: "No!"},
+                                    {name: "Available since", value: moment(found[0].viewableDate).format("dddd, MMMM Do of YYYY")},
+                                    {name: "Gone since", value: "Not yet known"}
+                                )
+                            }
+                        }
+                    }else if(lang === "ar"){
+
+                        //if there is no expire date
+                        if(found[0].expiryDate !== null){
+
+                            //if the bundle is available
                             if(found[0].available === true){
                                 bundle.addFields(
                                     {name: "متاحة للشراء", value: "نعم"},
@@ -97,7 +227,48 @@ module.exports = {
                                     {name: "غادرت منذ", value: moment(found[0].expiryDate).format("dddd, MMMM Do من YYYY")}
                                 )
                             }
+                        }else{
+                            if(found[0].available === true){
+                                bundle.addFields(
+                                    {name: "متاحة للشراء", value: "نعم"},
+                                    {name: "متاحة منذ", value: moment(found[0].viewableDate).format("dddd, MMMM Do من YYYY")},
+                                    {name: "سوف تغادر في", value: "لا يوجد تاريخ معلوم حتى الان"}
+                                )
+                            }else{
+                                bundle.addFields(
+                                    {name: "متاحة للشراء", value: "لا",},
+                                    {name: "متاحة منذ", value: moment(found[0].viewableDate).format("dddd, MMMM Do من YYYY")},
+                                    {name: "غادرت منذ", value: "لا يوجد تاريخ معلوم حتى الان"}
+                                )
+                            }
                         }
+                    }
+
+                    if(found[0].prices.length !== 0){
+
+                        //add cutsom prices SAR
+                        var pricesSA = {
+                            "paymentCurrencyCode": "SAR",
+                            "paymentCurrencySymbol": "SR",
+                            "paymentCurrencyAmountNatural": parseFloat(found[0].prices[1].paymentCurrencyAmountNatural * 3.75).toFixed(2)
+                        }
+
+                        //add sar
+                        bundle.addFields(
+                            {name: pricesSA.paymentCurrencyCode, value: pricesSA.paymentCurrencyAmountNatural + pricesSA.paymentCurrencySymbol, inline: true}
+                        )
+
+                        //add cutsom prices KWD
+                        var pricesKWD = {
+                            "paymentCurrencyCode": "KWD",
+                            "paymentCurrencySymbol": "KD",
+                            "paymentCurrencyAmountNatural": parseFloat(found[0].prices[1].paymentCurrencyAmountNatural * 0.30).toFixed(2)
+                        }
+
+                        //add kwd
+                        bundle.addFields(
+                            {name: pricesKWD.paymentCurrencyCode, value: pricesKWD.paymentCurrencyAmountNatural + pricesKWD.paymentCurrencySymbol, inline: true}
+                        )
 
                         //prices
                         for(let i = 0; i < found[0].prices.length; i++){
@@ -105,34 +276,43 @@ module.exports = {
                                 {name: found[0].prices[i].paymentCurrencyCode, value: found[0].prices[i].paymentCurrencyAmountNatural + found[0].prices[i].paymentCurrencySymbol, inline: true}
                             )
                         }
-
-                        //tumbnail
-                        if(found[0].displayAssets.length !== 0){
-                            bundle.setThumbnail(found[0].displayAssets[0].url)
-                        }
-
-                        //set the image
-                        bundle.setImage(found[0].thumbnail)
-
-                        message.channel.send(bundle)
-                        msg.delete()
                     }else{
-                        console.log("err")
+                        if(lang === "en"){
+                            bundle.addFields(
+                                {name: 'Prices', value: 'There is no prices yet'}
+                            )
+                        }else if(lang === "ar"){
+                            bundle.addFields(
+                                {name: 'الاسعار', value: 'لا يوجد اسعار حاليا'}
+                            )
+                        }
                     }
+
+                    //tumbnail and image
+                    if(found[0].displayAssets.length !== 0){
+
+                        //add thumbnail
+                        bundle.setThumbnail(found[0].displayAssets[0].url)
+
+                        //add the image
+                        if(found[0].thumbnail !== null){
+                            bundle.setImage(found[0].thumbnail)
+                        }else{
+                            bundle.setImage(found[0].displayAssets[0].background)
+                        }
+                    }else{
+
+                        //add the image
+                        if(found[0].thumbnail !== null){
+                            bundle.setImage(found[0].thumbnail)
+                        }
+                    }
+
+                    message.channel.send(bundle)
+                    msg.delete()
+
                 })
             })
-        }else{
-            if(lang === "en"){
-                const Err = new Discord.MessageEmbed()
-                .setColor('#BB00EE')
-                .setTitle(`No bundle has been found check your speling and try again ${errorEmoji}`)
-                message.reply(Err)
-            }else if(lang === "ar"){
-                const Err = new Discord.MessageEmbed()
-                .setColor('#BB00EE')
-                .setTitle(`لا يمكنني العثور على الحزمة الرجاء التأكد من كتابة الاسم بشكل صحيح ${errorEmoji}`)
-                message.reply(Err)
-            }
         }
     }
 }

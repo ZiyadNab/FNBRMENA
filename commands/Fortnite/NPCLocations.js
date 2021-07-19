c3ce15b00281f33f963cae7e3d74b9506cfdf9d0
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2021-07-19 04:37:57 +0300

    Update NPCLocations.js

--- commands/Fortnite/NPCLocations.js
+++ commands/Fortnite/NPCLocations.js
@@ -59,18 +59,18 @@ module.exports = {
                         }else{
 
                             //rotate
-                            ctx.translate(canvas.width / 2, canvas.height / 2)
-                            ctx.rotate(12)
                             ctx.translate(-canvas.width / 2, -canvas.height / 2)
+                            ctx.rotate(9.4)
+                            ctx.translate(canvas.width / 2, canvas.height / 2)
 
                             //add the pin based on the npc location
                             const npc = await Canvas.loadImage('./assets/NPC/pin.png')
                             ctx.drawImage(npc, x, (y + (180 * 2)), 160, 160)
 
                             //reset rotation
-                            ctx.translate(canvas.width / 2, canvas.height / 2)
-                            ctx.rotate(-12)
                             ctx.translate(-canvas.width / 2, -canvas.height / 2)
+                            ctx.rotate(-9.4)
+                            ctx.translate(canvas.width / 2, canvas.height / 2)
                         }
                     }
 

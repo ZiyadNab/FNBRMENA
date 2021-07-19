69f779f2ad6140cf691c5edd94a2d224edb5d179
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2021-07-19 05:53:27 +0300

    Update NPCLocations.js

--- commands/Fortnite/NPCLocations.js
+++ commands/Fortnite/NPCLocations.js
@@ -47,7 +47,7 @@ module.exports = {
                     for(let i = 0; i < res.data.npc.length; i++){
 
                         var x = res.data.npc[i].spawnLocations.locations[0].x - 85
-                        var y = res.data.npc[i].spawnLocations.locations[0].y - 160
+                        var y = res.data.npc[i].spawnLocations.locations[0].y - 165
 
                         //if the location is in range
                         if(y > 20){
@@ -62,7 +62,7 @@ module.exports = {
 
                             //add the pin based on the npc location
                             const npc = await Canvas.loadImage('./assets/NPC/flipedPIN.png')
-                            ctx.drawImage(npc, x, y + (160 / 2), 160, 160)
+                            ctx.drawImage(npc, x, y + 100, 160, 160)
 
                         }
                     }

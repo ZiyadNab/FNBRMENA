4bbc08157ae333ccfab24918d64a7663ba65e471
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2021-08-07 03:58:26 +0300

    Update NewSectionsEvents.js

--- Events/NewSectionsEvents.js
+++ Events/NewSectionsEvents.js
@@ -6,9 +6,11 @@ module.exports = (client, admin) => {
     const message = client.channels.cache.find(channel => channel.id === key.events.Section)
 
     //result
+    var lastModified = ""
     var response = []
-    var names = []
+    var sections = []
     var number = 0
+    var counter = 0
 
     const Sections = async () => {
 
@@ -16,109 +18,148 @@ module.exports = (client, admin) => {
         admin.database().ref("ERA's").child("Events").child("newsections").once('value', async function (data) {
 
             //store the data
-            var status = data.val().Active;
-            var lang = data.val().Lang;
-            var push = data.val().Push
+            var status = data.val().Active
+            var lang = data.val().Lang
+            var push = data.val().Push.Status
 
             //if the event is set to be true [ON]
             if(status === true){
 
                 //request data
-                axios.get(`https://fortniteapi.io/v2/shop/sections/list?lang=${lang}`, { headers: {'Content-Type': 'application/json','Authorization': "d4ce1562-839ff66b-3946ccb6-438eb9cf",} })
+                axios.get(`https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game?lang=${lang}`)
                 .then(async res => {
+
+                    //constant to make woring easy
+                    var sections = await res.data.shopSections.sectionList.sections
 
                     if(number === 0){
 
                         //store data
-                        for(let i = 0; i < res.data.sections.length; i++){
-                            names[i] = await res.data.sections[i].sectionId
+                        lastModified = await res.data.shopSections.lastModified
+                        for(let i = 0; i < sections.length; i++){
+                            response[i] = await sections[i]
                         }
 
                         //add sets names
                         number++
-
                     }
 
-                    //store data to response
-                    for(let i = 0; i < res.data.sections.length; i++){
-                        response[i] = await res.data.sections[i].sectionId
-                    }
+                    //if push was enabled
+                    if(push){
 
-                    if(push === true){
-                        names[0] = null
-                        names[1] = null
-                        names[2] = null
+                        //remove data
+                        lastModified = ""
+                        for(let i = 0; i < data.val().Push.Number; i++){
+                            response[i] = null
+                        }
                     }
-
+                
                     //check data
-                    if(JSON.stringify(names) !== JSON.stringify(response)){
+                    if(res.data.shopSections.lastModified !== lastModified){
 
-                        //inisilizing variavle
-                        var section = ""
-                        var counter = 0
-                        var added = 0
-                        var removed = 0
+                        //defin added string
+                        if(lang === "en") var stringAdded = "New sections just got added\n\n"
+                        else if(lang === "ar") var stringAdded = "تم اضافة اقسام جديدة\n\n"
 
-                        //loop throw every NEW section
+                        //defin removed string
+                        if(lang === "en") var stringRemoved = "Sections that just got removed\n\n"
+                        else if(lang === "ar") var stringRemoved = "اقسام تم حذفها\n\n"
+
+                        //added
+                        var added = []
+                        counter = 0
+                        for(let i = 0; i < sections.length; i++){
+
+                            //if the section at index i is new
+                            if(!response.includes(sections[i]) && sections[i] !== undefined && sections[i] !== null){
+
+                                //add the new sections to added array
+                                added[counter] = await sections[i]
+                                counter++
+                            }
+                        }
+
+                        //removed
+                        var removed = []
+                        counter = 0
                         for(let i = 0; i < response.length; i++){
 
-                            //check where is the new set
-                            if(!names.includes(response[i]) && response[i] !== null){
+                            //if the section at index i is removed
+                            if(!sections.includes(response[i]) && response[i] !== undefined && response[i] !== null){
+
+                                //add the removed sections to removed array
+                                removed[counter] = await response[i]
                                 counter++
-                                added++
-                                section += `\n• ${counter}: ${response[i]}`
                             }
                         }
 
-                        //loop throw every OLD section
-                        for(let i = 0; i < names.length; i++){
+                        //print all the added sections
+                        const printSections = async (Sections, String) => {
 
-                            //check where is the new set
-                            if(!response.includes(names[i]) && names[i] !== null){
-                                counter++
-                                removed++
-                                section += `\n• ${counter}: ${names[i]}`
+                            //get the section tabs and add them to a string
+                            while(Sections.length !== 0){
+
+                                //defined tabs and i index
+                                var tabs = 0
+                                var i = 0
+                                
+                                //see what is the index 0 is and how many tabs for the same section
+                                const firstIndex = await Sections[0]
+
+                                //loop throw all of the modified section
+                                while(i !== Sections.length){
+
+                                    //if there is another tab for the section at index 0
+                                    if(firstIndex.sectionDisplayName === Sections[i].sectionDisplayName){
+
+                                        //remove the section from the section array
+                                        const index = Sections.indexOf(Sections[i])
+                                        if(index > -1) Sections.splice(index, 1)
+
+                                        //add new tab
+                                        tabs++
+
+                                    } else i++
+                                }
+
+                                //add the tabs string
+                                if(firstIndex.sectionDisplayName !== undefined){
+
+                                    if(lang === "en") String += `${firstIndex.sectionDisplayName} | ${tabs} Tabs\n`
+                                    else if(lang === "ar") String += `${firstIndex.sectionDisplayName} | ${tabs} صفحة\n`
+                                }else{
+                                    
+                                    if(lang === "en") String += `${firstIndex.sectionId} | ${tabs} Tabs\n`
+                                    else if(lang === "ar") String += `${firstIndex.sectionId} | ${tabs} صفحة\n`
+                                }
                             }
+                                
+                            //create an embed for just added sections
+                            const addedEmbed = new Discord.MessageEmbed()
+
+                            //set the color
+                            addedEmbed.setColor('#00ffff')
+
+                            //set the description
+                            addedEmbed.setDescription(String)
+
+                            //send the new sections
+                            message.send(addedEmbed)
                         }
 
-                        //add title
-                        if(counter <= 1){
-                            if(lang === "en") section += `\n\n• ${counter} New section in total`
-                            else if(lang === "ar") section += `\n\n• بمجموع ${counter} قسم جديد!`
-                        }else{
-                            if(lang === "en") section += `\n\n• ${counter} New sections in total`
-                            else if(lang === "ar") section += `\n\n• بمجموع ${counter} أقسام جديدة!`
-                        }
-
-                        //create embed
-                        const sectionsInfo = new Discord.MessageEmbed()
-
-                        //add color
-                        sectionsInfo.setColor('#00ffff')
-
-                        //set title
-                        if(added >= 1){
-                            if(lang === "en") sectionsInfo.setTitle("All new sections that got added to the API")
-                            else if(lang === "ar") sectionsInfo.setTitle("جميع الأقسام الجديدة التي تمت اضافتها في الـ API")
-                        }else if(removed >= 1){
-                            if(lang === "en") sectionsInfo.setTitle("All new sections that got removed to the API")
-                            else if(lang === "ar") sectionsInfo.setTitle("جميع الأقسام المحذوفة التي تمت حذفها في الـ API")
-                        }
-
-                        //set description
-                        sectionsInfo.setDescription(section)
-
-                        //send message
-                        message.send(sectionsInfo)
+                        //if there is a new sections
+                        if(added.length > 0) await printSections(added, stringAdded)
+                        if(removed.length > 0) await printSections(removed, stringRemoved)
 
                         //store data
-                        for(let i = 0; i < res.data.sections.length; i++){
-                            names[i] = await res.data.sections[i].sectionId
+                        lastModified = await res.data.shopSections.lastModified
+                        for(let i = 0; i < sections.length; i++){
+                            response[i] = await sections[i]
                         }
 
                         //trun off push if enabled
-                        admin.database().ref("ERA's").child("Events").child("newsections").update({
-                            Push: false
+                        await admin.database().ref("ERA's").child("Events").child("newsections").child("Push").update({
+                            Status: false
                         })
 
                     }
@@ -129,5 +170,5 @@ module.exports = (client, admin) => {
             }
         })
     }
-    setInterval(Sections, 2 * 60000)
+    setInterval(Sections, 1 * 20000)
 }

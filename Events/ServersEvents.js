408d44cd3f66180db88ba441a333a0233f50a563
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2021-08-12 15:09:04 +0300

    Update ServersEvents.js

--- Events/ServersEvents.js
+++ Events/ServersEvents.js
@@ -15,32 +15,36 @@ module.exports = (client, admin) => {
         admin.database().ref("ERA's").child("Events").child("servers").once('value', async function (data) {
 
             //store the data
-            var status = data.val().Active;
-            var lang = data.val().Lang;
-            var push = data.val().Push
+            const status = data.val().Active
+            const lang = data.val().Lang
+            const push = data.val().Push
 
             //if the event is set to be true [ON]
-            if(status === true){
+            if(status){
+
+                //request the token
+                const token = await axios.get('https://api.nitestats.com/v1/epic/bearer')
 
                 //request data
-                axios.get('https://fn-api.com/api/status')
+                await axios.get('http://lightswitch-public-service-prod.ol.epicgames.com/lightswitch/api/service/fortnite/status',
+                {headers: {'Content-Type': 'application/json','Authorization': `Bearer ${token.data.accessToken}`}})
                 .then(async res => {
 
                     if(number === 0){
 
                         //store data
-                        response = await res.data.data.status
+                        response = await res.data.status
 
                         //add sets names
                         number++
                     }
 
-                    if(push === true){
+                    if(push){
                         response = []
                     }
 
                     //check data
-                    if(res.data.data.status !== response){
+                    if(res.data.status !== response){
 
                         //create embed
                         const servers = new Discord.MessageEmbed()
@@ -49,23 +53,27 @@ module.exports = (client, admin) => {
                         servers.setColor('#00ffff')
                         
                         //add title
-                        servers.setTitle('Fortnite servers are ' + res.data.data.status + '!')
+                        servers.setTitle('Fortnite servers are ' + res.data.status + '!')
 
                         //description string
                         var string = ""
-                        for(let i = 0; i < res.data.data.allowedActions.length; i++){
-                            string += res.data.data.allowedActions[i] + '\n'
+                        if(res.data.allowedActions.length !== 0){
+                            for(let i = 0; i < res.data.allowedActions.length; i++){
+                                string += `\`${res.data.data.allowedActions[i]}\n\``
+                            }
+                        }else{
+                            string += `\`No data\``
                         }
 
                         //add description
-                        servers.setDescription(res.data.data.message + '\n\n**Allowed Actions**\n' + string)
+                        servers.setDescription(res.data.message + '\n\n**Allowed Actions**\n' + string)
 
-                        if(res.data.data.status.toLowerCase() === "up") servers.setImage('https://i.imgur.com/kt4FLIq.jpg')
-                        else if(res.data.data.status.toLowerCase() === "down") servers.setImage('https://i.imgur.com/RBFGS9F.jpeg')
+                        if(res.data.status.toLowerCase() === "up") servers.setImage('https://imgur.com/Q6TA03N.png')
+                        else if(res.data.status.toLowerCase() === "down") servers.setImage('https://imgur.com/sm2JZhX.png')
                         else servers.setImage('https://i.imgur.com/OuV0nHn.jpg')
 
                         //add footer
-                        servers.setFooter(res.data.data.launcherInfo.appName)
+                        servers.setFooter(res.data.launcherInfoDTO.appName)
 
                         //send
                         message.send(servers)
@@ -76,14 +84,14 @@ module.exports = (client, admin) => {
                         })
 
                         //store data
-                        response = await res.data.data.status
+                        response = await res.data.status
                        
                     }
                 }).catch(err => {
-                    console.log("The issue is in Set Events ", err)
+                    console.log("The issue is in Servers Events ", err)
                 })
             }
         })
     }
-    setInterval(Server, 1 * 60000)
+    setInterval(Server, 1 * 10000)
 }

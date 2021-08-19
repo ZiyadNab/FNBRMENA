1e05b05905c2a7669fde847719788de2e3fd22f8
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2021-08-19 20:33:04 +0300

    Update NewSectionsEvents.js

--- Events/NewSectionsEvents.js
+++ Events/NewSectionsEvents.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js')
-const key = require('../Coinfigs/config.json');
-const axios = require("axios");
+const key = require('../Coinfigs/config.json')
 
 module.exports = (FNBRMENA, client, admin) => {
     const message = client.channels.cache.find(channel => channel.id === key.events.Section)
@@ -11,16 +10,15 @@ module.exports = (FNBRMENA, client, admin) => {
     var number = 0
     var counter = 0
 
+    //handle section
     const Sections = async () => {
 
         //checking if the bot on or off
         await admin.database().ref("ERA's").child("Events").child("newsections").once('value', async function (data) {
-
-            //store the data
-            var status = data.val().Active
-            var lang = data.val().Lang
-            var push = data.val().Push.Status
-            var type = data.val().Push.Type
+            const status = data.val().Active
+            const lang = data.val().Lang
+            const push = data.val().Push.Status
+            const type = data.val().Push.Type
 
             //if the event is set to be true [ON]
             if(status){
@@ -33,36 +31,17 @@ module.exports = (FNBRMENA, client, admin) => {
 
                         //store data
                         for(let i = 0; i < res.data.shopSections.sectionList.sections.length; i++){
-                            response[i] = await res.data.shopSections.sectionList.sections[i].sectionId
+                            response[counter++] = await res.data.shopSections.sectionList.sections[i]
                         }
-
-                        //add sets names
+                        counter = 0
                         number++
                     }
 
                     //up to date sections data
                     for(let i = 0; i < res.data.shopSections.sectionList.sections.length; i++){
-                        sections[i] = await res.data.shopSections.sectionList.sections[i].sectionId
+                        sections[counter++] = await res.data.shopSections.sectionList.sections[i]
                     }
 
-                    //if push was enabled
-                    if(push && type.toLowerCase() === "removed"){
-
-                        //remove data
-                        lastModified = ""
-                        for(let i = 0; i < data.val().Push.Number; i++){
-                            await sections.shift()
-                        }
-                        
-                    }else if(push && type.toLowerCase() === "added"){
-
-                        //remove data
-                        lastModified = ""
-                        for(let i = 0; i < data.val().Push.Number; i++){
-                            await response.shift()
-                        }
-                    }
-                
                     //check data
                     if(JSON.stringify(sections) !== JSON.stringify(response)){
 
@@ -80,19 +59,7 @@ module.exports = (FNBRMENA, client, admin) => {
                         for(let i = 0; i < sections.length; i++){
 
                             //if the section at index i is new
-                            if(!response.includes(sections[i])){
-
-                                //add the new sections to added array
-                                for(let x = 0; x < res.data.shopSections.sectionList.sections.length; x++){
-
-                                    //catch the searched sectionId
-                                    if(res.data.shopSections.sectionList.sections[i].sectionId === sections[i])
-                                    added[counter] = await res.data.shopSections.sectionList.sections[i]
-                                }
-
-                                //change the index
-                                counter++
-                            }
+                            if(!response.includes(sections[i])) added[counter++] = await sections[i]
                         }
 
                         //removed
@@ -101,19 +68,7 @@ module.exports = (FNBRMENA, client, admin) => {
                         for(let i = 0; i < response.length; i++){
 
                             //if the section at index i is removed
-                            if(!sections.includes(response[i])){
-
-                                //add the removed sections to removed array
-                                for(let x = 0; x < res.data.shopSections.sectionList.sections.length; x++){
-
-                                    //catch the searched sectionId
-                                    if(res.data.shopSections.sectionList.sections[i].sectionId === response[i])
-                                    removed[counter] = await res.data.shopSections.sectionList.sections[i]
-                                }
-
-                                //change the index
-                                counter++
-                            }
+                            if(!sections.includes(response[i])) removed[counter++] = await response[i]
                         }
 
                         //print all the added sections
@@ -176,7 +131,7 @@ module.exports = (FNBRMENA, client, admin) => {
 
                         //store data
                         for(let i = 0; i < res.data.shopSections.sectionList.sections.length; i++){
-                            response[i] = await res.data.shopSections.sectionList.sections[i].sectionId
+                            response[i] = await res.data.shopSections.sectionList.sections[i]
                         }
 
                         //trun off push if enabled

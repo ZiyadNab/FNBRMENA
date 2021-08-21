d21caf90be2fd243a1357edf5cdb2f5c8a7616b4
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2021-08-21 22:12:59 +0300

    fixed modified sections (=

--- Events/ModifiedSectionsEvents.js
+++ Events/ModifiedSectionsEvents.js
@@ -5,16 +5,15 @@ module.exports = (FNBRMENA, client, admin) => {
     const message = client.channels.cache.find(channel => channel.id === key.events.Section)
 
     //result
-    var response = []
-    var sections = []
-    var number = 0
-    var counter = 0
-
-    //handle section
-    const Sections = async () => {
+    var sectionsResponseData = []
+    var responseSectionId = []
+    var sectionsIdData = []
+    var counter = 1
+    
+    const modifiedSectionsHandler = async () => {
 
         //checking if the bot on or off
-        await admin.database().ref("ERA's").child("Events").child("newsections").once('value', async function (data) {
+        await admin.database().ref("ERA's").child("Events").child("modifiedsections").once('value', async function (data) {
             const status = data.val().Active
             const lang = data.val().Lang
             const push = data.val().Push.Status
@@ -27,23 +26,35 @@ module.exports = (FNBRMENA, client, admin) => {
                 await FNBRMENA.EpicContentEndpoint(lang)
                 .then(async res => {
 
-                    if(number === 0){
+                    if(counter === 1){
 
                         //store data
                         for(let i = 0; i < res.data.shopSections.sectionList.sections.length; i++){
-                            response[counter++] = await res.data.shopSections.sectionList.sections[i]
+                            sectionsResponseData[i] = await res.data.shopSections.sectionList.sections[i]
+                            responseSectionId[i] = await res.data.shopSections.sectionList.sections[i].sectionId
                         }
-                        counter = 0
-                        number++
+
+                        //add sets names
+                        counter--
                     }
 
                     //up to date sections data
                     for(let i = 0; i < res.data.shopSections.sectionList.sections.length; i++){
-                        sections[counter++] = await res.data.shopSections.sectionList.sections[i]
+                        sectionsIdData[i] = await res.data.shopSections.sectionList.sections[i].sectionId
                     }
 
+                    //if push was enabled
+                    if(push){
+
+                        //remove data
+                        for(let i = 0; i < data.val().Push.Number; i++){
+                            if(type.toLowerCase() === "removed") await sectionsIdData.shift()
+                            if(type.toLowerCase() === "added") await responseSectionId.shift()
+                        }
+                    }
+                
                     //check data
-                    if(JSON.stringify(sections) !== JSON.stringify(response)){
+                    if(JSON.stringify(sectionsIdData) !== JSON.stringify(responseSectionId)){
 
                         //defin added string
                         if(lang === "en") var stringAdded = "New sections just got added\n\n"
@@ -56,19 +67,37 @@ module.exports = (FNBRMENA, client, admin) => {
                         //added
                         var added = []
                         counter = 0
-                        for(let i = 0; i < sections.length; i++){
+                        for(let i = 0; i < sectionsIdData.length; i++){
 
                             //if the section at index i is new
-                            if(!response.includes(sections[i])) added[counter++] = await sections[i]
+                            if(!responseSectionId.includes(sectionsIdData[i])){
+
+                                //add the new sections to added array
+                                for(let x = 0; x < res.data.shopSections.sectionList.sections.length; x++){
+
+                                    //catch the searched sectionId
+                                    if(res.data.shopSections.sectionList.sections[x].sectionId === sectionsIdData[i])
+                                    added[counter++] = await res.data.shopSections.sectionList.sections[x]
+                                }
+                            }
                         }
 
                         //removed
                         var removed = []
                         counter = 0
-                        for(let i = 0; i < response.length; i++){
+                        for(let i = 0; i < responseSectionId.length; i++){
 
                             //if the section at index i is removed
-                            if(!sections.includes(response[i])) removed[counter++] = await response[i]
+                            if(!sectionsIdData.includes(responseSectionId[i])){
+
+                                //add the removed sections to removed array
+                                for(let x = 0; x < sectionsResponseData.length; x++){
+
+                                    //catch the searched sectionId
+                                    if(sectionsResponseData[x].sectionId === responseSectionId[i])
+                                    removed[counter++] = await sectionsResponseData[x]
+                                }
+                            }
                         }
 
                         //print all the added sections
@@ -131,21 +160,22 @@ module.exports = (FNBRMENA, client, admin) => {
 
                         //store data
                         for(let i = 0; i < res.data.shopSections.sectionList.sections.length; i++){
-                            response[i] = await res.data.shopSections.sectionList.sections[i]
+                            sectionsResponseData[i] = await res.data.shopSections.sectionList.sections[i]
+                            responseSectionId[i] = await res.data.shopSections.sectionList.sections[i].sectionId
                         }
 
                         //trun off push if enabled
-                        await admin.database().ref("ERA's").child("Events").child("newsections").child("Push").update({
+                        await admin.database().ref("ERA's").child("Events").child("modifiedsections").child("Push").update({
                             Status: false
                         })
 
                     }
                     
                 }).catch(err => {
-                    console.log("The issue is in New Sections Events ", err)
+                    console.log("The issue is in Modified Sections Events ", err)
                 })
             }
         })
     }
-    setInterval(Sections, 1 * 20000)
+    setInterval(modifiedSectionsHandler, 1 * 10000)
 }

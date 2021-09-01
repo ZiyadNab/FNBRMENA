0c574b5c6ede7cbef4dbc165c942645db0c448d1
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2021-09-01 17:30:57 +0300

    Update tournamentDetails.js

--- commands/Fortnite/tournamentDetails.js
+++ commands/Fortnite/tournamentDetails.js
@@ -18,7 +18,7 @@ module.exports = {
     callback: async (message, args, text, Discord, client, admin, alias, errorEmoji, checkEmoji, loadingEmoji) => {
 
         //get the user language from the database
-        const lang = await FNBRMENA.Admin(admin, message, "", "Lang")
+        const lang = await FNBRMENA.Admin(admin, message, "", "Lang");
 
         //request data by the windowID
         FNBRMENA.tournamentSessions(text)
@@ -28,44 +28,55 @@ module.exports = {
             if(res.data.result){
                 
                 //get the actual tornament id from Comp Calendar Endpoint
-                const calendarEndpointResponse = await FNBRMENA.CompCalendarEndpoint(lang)
+                const calendarEndpointResponse = await FNBRMENA.CompCalendarEndpoint(lang);
 
                 //get the actual tornament id from Content Endpoint
-                const contentEndpointResponse = await FNBRMENA.EpicContentEndpoint(lang)
+                const contentEndpointResponse = await FNBRMENA.EpicContentEndpoint(lang);
 
                 //loop and find the object that matches the given eventId to get extended data
                 let searchedCalendarTournamentObj = []
                 for(let i = 0; i < calendarEndpointResponse.data.eventsData.length; i++){
                     if(await calendarEndpointResponse.data.eventsData[i].eventId === await res.data.session.eventId){
-                        searchedCalendarTournamentObj.push(calendarEndpointResponse.data.eventsData[i])
-                    }
-                }
+                        searchedCalendarTournamentObj.push(calendarEndpointResponse.data.eventsData[i]);
+                    };
+                };
 
                 //loop and find the object that matches the given eventId to get extended data
-                let searchedConntentTournamentObj = []
+                let searchedContentTournamentObj = []
                 for(let i = 0; i < contentEndpointResponse.data.tournamentinformation.tournament_info.tournaments.length; i++){
                     if(await contentEndpointResponse.data.tournamentinformation.tournament_info.tournaments[i].tournament_display_id === await res.data.session.eventDisplayId){
-                        searchedConntentTournamentObj.push(contentEndpointResponse.data.tournamentinformation.tournament_info.tournaments[i])
-                    }
-                }
+                        searchedContentTournamentObj.push(contentEndpointResponse.data.tournamentinformation.tournament_info.tournaments[i]);
+                    };
+                };
 
                     //loading message
-                    const generating = new Discord.MessageEmbed()
-                    generating.setColor(FNBRMENA.Colors("embed"))
-                    if(lang === "en") generating.setTitle(`Loading ${searchedConntentTournamentObj[0].long_format_title} results... ${loadingEmoji}`)
-                    if(lang === "ar") generating.setTitle(`جاري تحميل احصائيات بطولة ${searchedConntentTournamentObj[0].long_format_title}... ${loadingEmoji}`)
+                    const generating = new Discord.MessageEmbed();
+                    generating.setColor(FNBRMENA.Colors("embed"));
+                    if(lang === "en") generating.setTitle(`Loading ${searchedContentTournamentObj[0].long_format_title} results... ${loadingEmoji}`);
+                    if(lang === "ar") generating.setTitle(`جاري تحميل احصائيات بطولة ${searchedContentTournamentObj[0].long_format_title}... ${loadingEmoji}`);
                     message.channel.send(generating)
                     .then( async msg => {
 
                     //start working with the data
                     //...
 
+                    //hieght, x, y and numberOfRanks measures
+                    let height = 480 + 160;
+                    let numberOfRanks = 10;
+                    var x = 150;
+                    var y = 480;
+
+                    //loop throw every rank
+                    for(let i = 0; i < numberOfRanks; i++){
+                        height += 90 + 20;
+                    }
+
                     //registering fonts
                     Canvas.registerFont('./assets/font/Lalezar-Regular.ttf', {family: 'Arabic',weight: "700",style: "bold"});
-                    Canvas.registerFont('./assets/font/BurbankBigCondensed-Black.otf' ,{family: 'Burbank Big Condensed',weight: "700",style: "bold"})
+                    Canvas.registerFont('./assets/font/BurbankBigCondensed-Black.otf' ,{family: 'Burbank Big Condensed',weight: "700",style: "bold"});
 
                     //creating canvas
-                    const canvas = Canvas.createCanvas(1920, 1080);
+                    const canvas = Canvas.createCanvas(2800, height);
                     const ctx = canvas.getContext('2d');
 
                     //create background grediant
@@ -77,23 +88,77 @@ module.exports = {
                      * From Content Endpoint
                     **/
 
-                    grediant.addColorStop(0, `#${searchedConntentTournamentObj[0].background_left_color}`)
-                    grediant.addColorStop(1, `#${searchedConntentTournamentObj[0].background_right_color}`)
+                    grediant.addColorStop(0, `#${searchedContentTournamentObj[0].background_left_color}`);
+                    grediant.addColorStop(1, `#${searchedContentTournamentObj[0].background_right_color}`);
 
                     //add the background color to ctx
-                    ctx.fillStyle = grediant
-                    ctx.fillRect(0, 0, canvas.width, canvas.height)
+                    ctx.fillStyle = grediant;
+                    ctx.fillRect(0, 0, canvas.width, canvas.height);
+                    
+                    //loop throw every top ${numberOfRanks} number
+                    for(let i = 0; i < numberOfRanks; i++){
 
-                    //credits
-                    ctx.fillStyle = '#ffffff';
-                    ctx.textAlign='left';
-                    ctx.font = '50px Burbank Big Condensed'
-                    ctx.fillText("FNBRMENA", 15, 55)
+                        //add the color to ctx
+                        ctx.fillStyle = `#${searchedContentTournamentObj[0].shadow_color}`;
+
+                        //draw the line
+                        ctx.fillRect(x, y, canvas.width - 300, 90);
+
+                        //change the y value
+                        y += 90 + 20
+
+                    } y = 480
+
+                    //add the sideways lines for the stats [language matters]
+                    if(lang === "en"){
+
+                        //change the x value
+                        x += 40
+
+                        //add the color to ctx
+                        ctx.fillStyle = `#${searchedContentTournamentObj[0].secondary_color}`;
+
+                        //draw the line
+                        ctx.fillRect(x, (y - 40), 90, (canvas.height - y) + 40);
+
+                        //change the x value
+                        x += 1050
+
+                        //loop throw 4 indexes
+                        for(let i = 0; i < 4; i++){
+
+                            //add the color to ctx
+                            ctx.fillStyle = `#${searchedContentTournamentObj[0].secondary_color}`;
+
+                            //draw the line
+                            ctx.fillRect(x, (y - 40), 140, (canvas.height - y) + 40);
+
+                            //change the x value
+                            x += 100 + 140
+
+                        } x = 150
+
+                        //add the ppl data
+                        for(let i = 0; i < numberOfRanks; i++){
+
+                            //chenge the color to white
+                            ctx.fillStyle = '#ffffff'
+                            ctx.textAlign='center'
+                            ctx.font = '50px Burbank Big Condensed'
+
+                            //first thing draw the user rank
+                            ctx.fillText(i + 1, x + 85, y + 60)
+
+                            y += 90 + 20
+
+                        }
+
+                    }
 
                     //send
-                    const att = new Discord.MessageAttachment(canvas.toBuffer(), `${res.data.session.windowId}.png`)
-                    await message.channel.send(att)
-                    msg.delete()
+                    const att = new Discord.MessageAttachment(canvas.toBuffer(), `${res.data.session.windowId}.png`);
+                    await message.channel.send(att);
+                    msg.delete();
                     
                 })
             }

beb967ca0e0d2a279b53726c8ef07be6aea0a02d
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2021-09-04 14:56:09 +0300

    Update Music.js

--- commands/Fortnite/Music.js
+++ commands/Fortnite/Music.js
@@ -21,12 +21,22 @@ module.exports = {
 
         //inisilizing data
         var SearchType = "name"
+        var musicType = "video"
 
         //if input is an id
         if(text.includes("_")) SearchType = "id"
 
+        //if there is + in the message
+        if(text.includes("+")){
+
+           //extract the music pack from the text string
+           var musicPackNameOrID = text.substring(0, text.indexOf("+"))
+           musicType = text.substring(text.indexOf("+") + 1, text.length).trim()
+
+       }else var musicPackNameOrID = text
+
         //request the emote video
-        FNBRMENA.SearchByType(lang, text, 'music', SearchType)
+        FNBRMENA.SearchByType(lang, musicPackNameOrID, 'music', SearchType)
         .then(async res => {
 
             //check if the user entered a valid emote name
@@ -40,48 +50,49 @@ module.exports = {
                         //send the generating message
                         const generating = new Discord.MessageEmbed()
                         generating.setColor(FNBRMENA.Colors("embed"))
-                        if(lang === "en") generating.setTitle(`Loading the music information ${loadingEmoji}`)
-                        else if(lang === "ar") generating.setTitle(`جاري تحميل بيانات اغنية اللوبي ${loadingEmoji}`)
+                        if(lang === "en") generating.setTitle(`Loading the ${res.data.items[0].name}... ${loadingEmoji}`)
+                        else if(lang === "ar") generating.setTitle(`جاري تحميل بيانات ${res.data.items[0].name}... ${loadingEmoji}`)
                         message.channel.send(generating)
                         .then( async msg => {
 
-                            //send attatchment
-                            const att = await new Discord.MessageAttachment(res.data.items[0].video)
+                            try{
 
-                            //send the emote video
-                            await message.channel.send(att)
-                            msg.delete()
+                                //send attatchment
+                                if(musicType.toLocaleLowerCase() === "video") var att = await new Discord.MessageAttachment(res.data.items[0].video)
+                                else if(musicType.toLocaleLowerCase() === "mp3") var att = await new Discord.MessageAttachment(res.data.items[0].audio)
+
+                                //send the emote video
+                                await message.channel.send(att)
+                                msg.delete()
+
+                            }catch{
+
+                                //send the emote video
+                                if(musicType.toLocaleLowerCase() === "video") await message.channel.send(res.data.items[0].video)
+                                else if(musicType.toLocaleLowerCase() === "mp3") await message.channel.send(res.data.items[0].audio)
+                                msg.delete()
+                            }
                         })
                     }else{
 
                         //create embed
                         const err = new Discord.MessageEmbed()
-
-                        //add the color
                         err.setColor(FNBRMENA.Colors("embed"))
-
-                        //set the title
-                        if(lang === "en"){
-                            err.setTitle(`There is no mp3 file for this music pack yet ${errorEmoji}`)
-                        }else if(lang === "ar"){
-                            err.setTitle(`لا يوجد ملف صوت ${errorEmoji}`)
-                        }
+                        if(lang === "en") err.setTitle(`There is no mp3 file for this music pack yet ${errorEmoji}`)
+                        else if(lang === "ar") err.setTitle(`لا يوجد ملف صوت ${errorEmoji}`)
+                        
 
                         //send the error message
                         message.channel.send(err)
                     }
                 }else{
-                    if(lang === "en"){
-                        const Err = new Discord.MessageEmbed()
-                        .setColor(FNBRMENA.Colors("embed"))
-                        .setTitle(`The type of the item is not a music pack ${errorEmoji}`)
-                        message.reply(Err)
-                    }else if(lang === "ar"){
-                        const Err = new Discord.MessageEmbed()
-                        .setColor(FNBRMENA.Colors("embed"))
-                        .setTitle(`يجب عليك البحث عن ميوزك باك ${errorEmoji}`)
-                        message.reply(Err)
-                    }
+
+                    //wrong item type
+                    const Err = new Discord.MessageEmbed()
+                    Err.setColor(FNBRMENA.Colors("embed"))
+                    if(lang === "en") Err.setTitle(`The type of the item is not a music pack ${errorEmoji}`)
+                    else if(lang === "ar") Err.setTitle(`يجب عليك البحث عن ميوزك باك ${errorEmoji}`)
+                    message.reply(Err)
                 }
             }else{
                 
@@ -95,6 +106,12 @@ module.exports = {
 
         }).catch(err => {
 
+            //request error
+            const Err = new Discord.MessageEmbed()
+            Err.setColor(FNBRMENA.Colors("embed"))
+            if(lang === "en") Err.setTitle(`An error happened while getting the data please try again later. ${errorEmoji}`)
+            else if(lang === "ar") Err.setTitle(`عذرا. لقد حدث مشكلة اثناء استخراج البيانات الرجاء المحاولةة لاحقا ${errorEmoji}`)
+            message.channel.send(Err)
         })
     }
 }

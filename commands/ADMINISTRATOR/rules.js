4b6317ae0c5e9a447b43af3f37342f60abccad68
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2021-09-11 21:29:37 +0300

    aa

--- commands/ADMINISTRATOR/rules.js
+++ commands/ADMINISTRATOR/rules.js
@@ -7,32 +7,37 @@ module.exports = {
     maxArgs: null,
     cooldown: -1,
     permissionError: 'Sorry you do not have acccess to this command',
-    callback: (FNBRMENA, message, args, text, Discord, client, admin, alias, errorEmoji, checkEmoji, loadingEmoji, greenStatus, redStatus) => {
-        admin.database().ref("ERA's").child("Users").child(message.author.id).once('value', async function (data) {
-            var lang = data.val().lang;
-            const messageAnnounce = new Discord.MessageEmbed()
-            messageAnnounce.setColor(FNBRMENA.Colors("embed"))
-            messageAnnounce.setTitle(args.join(' '))
-            const accounce = client.channels.cache.find(channel => channel.id === config.channels.rules)
-            if(accounce.send(messageAnnounce)){
-                const messageAnnounceDone = new Discord.MessageEmbed()
-                messageAnnounceDone.setColor(FNBRMENA.Colors("embed"))
-                if(lang === "en"){
-                    messageAnnounceDone.setTitle(`The rule has been published ${checkEmoji}`)
-                }else if(lang === "ar"){
-                    messageAnnounceDone.setTitle(`تم نشر القانون ${checkEmoji}`)
-                }
-                message.channel.send(messageAnnounceDone)
-            }else{
-                const messageAnnounceNotDone = new Discord.MessageEmbed()
-                messageAnnounceNotDone.setColor(FNBRMENA.Colors("embed"))
-                if(lang === "en"){
-                    messageAnnounceNotDone.setTitle(`There was an error publishing this قعمث ${errorEmoji}`)
-                }else if(lang === "ar"){
-                    messageAnnounceNotDone.setTitle(`يوجد مشكلة في عملية نشر القانون ${errorEmoji}`)
-                }
-                message.channel.send(messageAnnounceNotDone)
-            }
+    callback: async (FNBRMENA, message, args, text, Discord, client, admin, alias, errorEmoji, checkEmoji, loadingEmoji, greenStatus, redStatus) => {
+
+        //get the user language from the database
+        const lang = await FNBRMENA.Admin(admin, message, "", "Lang")
+
+        //get rules channel id
+        const rulesMessage = await client.channels.cache.find(channel => channel.id === config.channels.rules)
+
+        //inislizing embed
+        const rulesEmbed = new Discord.MessageEmbed()
+        rulesEmbed.setColor(FNBRMENA.Colors("embed"))
+        rulesEmbed.setDescription(text)
+
+        //send the message
+        rulesMessage.send(rulesEmbed)
+        .then(() => {
+
+            //inislizing embed
+            const successfullySent = new Discord.MessageEmbed()
+            successfullySent.setColor(FNBRMENA.Colors("embed"))
+            if(lang === "en") successfullySent.setTitle(`Rules has been successfully sent ${checkEmoji}`)
+            else if(lang === "ar") successfullySent.setTitle(`تم نشر القوانين بنجاح ${checkEmoji}`)
+            message.channel.send(successfullySent)
+        }).catch(() => {
+
+            //inislizing embed
+            const errSent = new Discord.MessageEmbed()
+            errSent.setColor(FNBRMENA.Colors("embed"))
+            if(lang === "en") errSent.setTitle(`There was an error while sending the message please ask the Owner for help ${checkEmoji}`)
+            else if(lang === "ar") errSent.setTitle(`يوجد مشكلة اثناء نشر الأوامر الرجاء التواصل مع الـ Owner لحل المشكلة ${checkEmoji}`)
+            message.channel.send(errSent)
         })
-    },
+    }
 }

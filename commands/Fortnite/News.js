0a3cf3b3f5e284b4c501c007c2d3cb69a9091a42
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2022-09-16 13:56:30 +0300

    Update News.js

--- commands/Fortnite/News.js
+++ commands/Fortnite/News.js
@@ -1,8 +1,6 @@
 const Canvas = require('canvas')
 const wrap = require('word-wrap')
 const Gif = require('gif-encoder-2')
-const Data = require('../FNBRMENA')
-const FNBRMENA = new Data()
 
 module.exports = {
     commands: 'news',
@@ -271,6 +269,8 @@ module.exports = {
 
 process.on("message", async (data) => {
     // console.log("Child PID: " + process.pid)
+    const Data = require('../../FNBRMENA')
+    const FNBRMENA = new Data()
 
     var callback = new Function('return ' + data.callback)();
     callback(FNBRMENA, data.message, data.args, data.text, require('discord.js'), "", "", data.userData, data.alias, data.emojisObject)

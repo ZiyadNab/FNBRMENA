3a7a5febec3e0c355fd5b9719746341557cb9784
ZiyadNab <77915132+ZiyadNab@users.noreply.github.com>
2022-09-23 15:54:23 +0300

    Update Weapons.js

--- commands/Fortnite/Weapons.js
+++ commands/Fortnite/Weapons.js
@@ -102,7 +102,7 @@ module.exports = {
                 // Weapon status
                 if(res.enabled) var status = await Canvas.loadImage('https://i.imgur.com/j2VNlek.png')
                 else var status = await Canvas.loadImage('https://imgur.com/LlfZAAy.png')
-                ctx.drawImage(status, 100, 100, 10, 10)
+                ctx.drawImage(status, 10, 10, 100, 100)
 
                 // Add weapon name
                 ctx.fillStyle = '#ffffff'

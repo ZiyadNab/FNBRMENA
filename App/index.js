bd5fbfe3895ed8d9950a7e4e26f6c9545e210ed1
ZiyadNab <ziyadnabil050@gmail.com>
2024-09-01 22:41:06 +0300

    Fixed Discord Token

--- App/index.js
+++ App/index.js
@@ -14,6 +14,21 @@ const FNBRMENA = new Data()
 const fs = require('fs')
 const { Player } = require('discord-player')
 
+console.log({
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain
+});
+
+
 // Get access to the firebase database
 admin.initializeApp({
   credential: admin.credential.cert({

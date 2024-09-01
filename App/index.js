31d76da39fb57a39199097df7d7f539a85f08144
ZiyadNab <ziyadnabil050@gmail.com>
2024-09-01 23:18:53 +0300

    Firebase Config

--- App/index.js
+++ App/index.js
@@ -15,22 +15,20 @@ const fs = require('fs')
 const { Player } = require('discord-player')
 
 // Get access to the firebase database
-const serviceAccount = JSON.parse({
-  type: process.env.type,
-  project_id: process.env.project_id,
-  private_key_id: process.env.private_key_id,
-  private_key: process.env.private_key,
-  client_email: process.env.client_email,
-  client_id: process.env.client_id,
-  auth_uri: process.env.auth_uri,
-  token_uri: process.env.token_uri,
-  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-  client_x509_cert_url: process.env.client_x509_cert_url,
-  universe_domain: process.env.universe_domain
-})
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key.replace(/\\n/g, '\n'),
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+    universe_domain: process.env.universe_domain
+  }),
   databaseURL: "https://fnbrmena-bot-default-rtdb.europe-west1.firebasedatabase.app/",
   storageBucket: 'gs://fnbrmena-bot.appspot.com'
 })
